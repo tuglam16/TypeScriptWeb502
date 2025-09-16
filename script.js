578e906dd1aea9080e64d58f87da13caea6e1c5b
lamnt <nguyenlam.ivi@gmail.com>
2025-09-16 20:11:09 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function CvHCN(dai, rong) {
+    return (dai + rong) * 2;
+}
+var dai = 10;
+var rong = 5;
+console.log("chu vi hinh chu nhat: ", CvHCN(dai, rong));
---
+++ script.ts
@@ -0,0 +1,6 @@
+function CvHCN (dai:number , rong:number): number {
+    return (dai + rong) *2;
+}
+const dai = 10;
+const rong = 5;
+console.log("chu vi hinh chu nhat: ", CvHCN(dai,rong));
