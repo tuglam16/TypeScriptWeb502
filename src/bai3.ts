c3b229e4190166d052f341d84ee81cd593e7bbf5
tuglam16 <nguyentunglam1216@gmail.com>
2025-09-20 20:03:58 +0700

    buoi2 update

--- src/bai3.ts
+++ src/bai3.ts
@@ -1,9 +1,14 @@
 function countChar(str: string, char: string): number {
+const lowerStr = str.toLowerCase();
+  const lowerChar = char.toLowerCase();
+
   let count = 0;
-  for (let c of str) {
-    if (c === char) count++;
+  for (let i = 0; i < lowerStr.length; i++) {
+    if (lowerStr[i] === lowerChar) {
+      count++;
+    }
   }
   return count;
 }
-console.log(countChar("hello world", "o"));
-console.log(countChar("typescript", "t")); 
+console.log(countChar("Hello World", "l"));
+console.log(countChar("Hello World", "L")); 
