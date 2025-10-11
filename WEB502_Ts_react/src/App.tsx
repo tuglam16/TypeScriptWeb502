79c06631e1ac5b9862bf977cfd6e0ccd79b11b2e
tuglam16 <nguyentunglam1216@gmail.com>
2025-10-11 19:43:54 +0700

    List

--- WEB502_Ts_react/src/App.tsx
+++ WEB502_Ts_react/src/App.tsx
@@ -2,9 +2,18 @@
 // import Button from "./components/Button";
 // import MyUseState from "./components/MyUseState";
 import { Route, Router, Routes } from "react-router-dom";
-import ProductList from "./pages/ProductList";
-import ProductDetail from "./pages/ProductDetail";
+// import ProductList from "./pages/ProductList";
+// import ProductDetail from "./pages/ProductDetail";
 import Users from "./components/Users";
+import { Toaster } from "react-hot-toast";
+import Edit from "./pages/Edit";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import AdminLayout from "./layout/AdminLayout";
+import Home from "./pages/Home";
+import ClientLayout from "./layout/ClientLayout";
+import ProductList from "./pages/ProductList";
+
 
 function App() {
   return (
@@ -13,12 +22,23 @@ function App() {
       <Button text="Random Color" />
     <MyUseState/> */}
 
-      <Routes>
-        <Route path="/" element={<ProductList />} />
-        <Route path="/products/:id" element={<ProductDetail />} />
-        <Route path="/useeffect" element={<Users />} />
-      </Routes>
+     <Routes>
+        {/* Client Layout */}
+        <Route path="/" element={<ClientLayout />}>
+          <Route index element={<Home />} />
+          <Route path="users" element={<Users />} />
+          <Route path="products" element={<ProductList />} />
+          <Route path="products/:id" element={<ProductList />} />
+        </Route>
 
+        {/* Admin Layout */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="list" element={<List />}></Route>
+          <Route path="add" element={<Add />}></Route>
+          <Route path="edit" element={<Edit />}></Route>
+        </Route>
+      </Routes>
+      <Toaster />
     </>
   );
 }
